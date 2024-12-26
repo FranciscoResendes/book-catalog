1d3b59ae78f9b811e45f45ca3b8b0351fa8ff948
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2024-12-26 10:55:48 +0000

    fix to api url

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './Book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private api = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getAllBooks(): Observable<Book> {
+    return this.http.get<Book>(`${this.api}/books`);
+  }
+
+  public login(credentials: any) {
+    return this.http.post(`${this.api}/user`, credentials);
+  }
 }
