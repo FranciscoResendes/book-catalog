ae2dc39572b9c0440a8b72f040575b22336182ed
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2024-12-26 13:34:28 +0000

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
