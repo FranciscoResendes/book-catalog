32237861a2a4d4a7f9eec66533aa01c5f12973d6
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2024-12-26 10:56:03 +0000

    added post mapping for login

--- backend/catalog/src/main/java/books/catalog/controller/RestApiController.java
+++ backend/catalog/src/main/java/books/catalog/controller/RestApiController.java
@@ -2,21 +2,37 @@ package books.catalog.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import books.catalog.entities.Book;
+import books.catalog.entities.Users;
+import books.catalog.service.BookService;
+import books.catalog.service.UsersService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class RestApiController {
+
+    @Autowired BookService bookService;
+    @Autowired UsersService usersService;
     
     @GetMapping
     public ResponseEntity<List<Book>> getBooks() {
         List<Book> books = null;
         return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<Users> getUser(@RequestBody Users credentials) {
+        Users user = usersService.checkUser(credentials.getUsername(), credentials.getPassword());
+        return  user != null ? 
+            new ResponseEntity<>(user, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 }
