777e5398bc563811ada7393e09b3d7d5e904e616
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2024-12-27 14:28:58 +0000

    created user component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: '', component: MainComponent}
+  {path: '', component: MainComponent},
+  {path: 'user', component: UserComponent}
 ];
 
 @NgModule({
