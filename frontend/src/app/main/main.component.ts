801610153f53cc78a745ea730abd16e918c16f3d
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2025-01-01 23:40:55 +0000

    added form to home page

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Book } from '../Book';
 import { ApiService } from '../api.service';
-import { lastValueFrom } from 'rxjs';
+import { generate, lastValueFrom } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-main',
@@ -14,7 +16,7 @@ export class MainComponent {
   public selectedBook?: Book = undefined;
   public showPopup: boolean = false;
 
-  constructor(private apiServ: ApiService){}
+  constructor(private apiServ: ApiService, public authService: AuthService){}
 
   ngOnInit(){
     this.getBooks();
@@ -34,8 +36,31 @@ export class MainComponent {
     this.showPopup = true;
   }
 
-  submitAddBook(){
-
+  submitAddBook(frm: NgForm){
+    console.log(frm.value);
+    const book = {
+      id: this.selectedBook?.id ?? 0,
+      //img: this.selectedBook?.img, ?? ''
+      isbn: this.selectedBook?.isbn ?? '',
+      title: frm.value.title,
+      author: this.selectedBook?.author ?? '',
+      genre: this.selectedBook?.genre ?? '',
+      status: frm.value.status,
+      chaptersRead: frm.value.chaptersRead,
+      pages: this.selectedBook?.pages ?? 0,
+      score: this.selectedBook?.score ?? 0,
+    };
+    const jwt = this.authService.isLoggedIn();
+    if (jwt) {
+      this.apiServ.addBook(jwt, book).subscribe(response => {
+        if (response) {
+          console.log(response);
+        }
+      });
+    }
+    else{
+      console.log("You are not logged in");
+    } 
   }
 
   closePopup(){
