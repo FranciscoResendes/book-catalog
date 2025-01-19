c5666108ed0efdd0b89514cc7bf73bf9e784ce73
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2025-01-19 13:37:00 +0000

    Create AuthorService.java

---
+++ backend/catalog/src/main/java/books/catalog/service/AuthorService.java
@@ -0,0 +1,27 @@
+package books.catalog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import books.catalog.entities.Author;
+import books.catalog.repositories.AuthorRepository;
+
+@Service
+public class AuthorService {
+    
+    @Autowired AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRep) {
+        this.authorRepository = authorRep;
+    }
+
+    public void updateAuthor(Author author) {
+        authorRepository.save(author);
+    }
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+}
