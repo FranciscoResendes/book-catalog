dd3e427fb9061e370e8a738436ec67464840d7ad
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2025-01-20 22:31:55 +0000

    nav bar is removed if on login page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ export class AppComponent {
   title = 'book-catalog';
 
   constructor(public authService: AuthService){}
+
+  isLoginPage(){
+    return window.location.pathname === '/login';
+  }
 }
