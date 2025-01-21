3e62fd45113b4d86146f390200c06cc4df33828b
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2025-01-21 23:43:22 +0000

    Added register form

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private authService: AuthService, private router: Router){}
+
+  register(form: NgForm){
+    const user = {
+      username: form.value.username,
+      email: form.value.email,
+      password: form.value.password,
+      role: 'ROLE_USER',
+      books: [],
+      date: new Date()
+    };
+    this.authService.register(user).subscribe(isValid => {
+      if (!isValid) {
+        console.log('Error');
+      }
+      else this.router.navigate(['/login']);
+    });
+  }
+  
+    goBack(){
+      this.router.navigate(['/']);
+    }
 }
