ac5bd274892864c2d02bb28821e2b3def93e3c3e
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2025-01-27 22:48:14 +0000

    Create DriverManager.java

---
+++ tests/src/test/java/util/DriverManager.java
@@ -0,0 +1,17 @@
+package util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverManager {
+    static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        WebDriverManager.chromedriver().driverVersion("132.0.6834.111").setup();
+        driver = new ChromeDriver();
+        return driver;
+    }
+    
+}
