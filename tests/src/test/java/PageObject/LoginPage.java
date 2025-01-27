fa2c748b554444f0fe8b28ae1f5dd2882fda3b21
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2025-01-27 22:50:34 +0000

    Update login page and tests for best practices
    - Still missing the @Then method implementation.

--- tests/src/test/java/PageObject/LoginPage.java
+++ tests/src/test/java/PageObject/LoginPage.java
@@ -1,10 +1,12 @@
 package PageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
 public class LoginPage {
     private static final String URL = "http://localhost:4200/";
+    private static final String loginButton = "//a[contains(text(),'Login')]";
     WebDriver driver;
 
     public LoginPage(WebDriver driver) {
@@ -16,4 +18,13 @@ public class LoginPage {
         driver.manage().window().maximize();
     }
     
+    public void goToLoginPage() {
+        driver.findElement(By.xpath(loginButton)).click();
+    }
+
+    public void enterLoginCredentials(String username, String password) {
+        driver.findElement(By.id("loginUsername")).sendKeys(username);
+        driver.findElement(By.id("loginPassword")).sendKeys(password);
+        driver.findElement(By.id("loginButton")).click();
+    }
 }
--- tests/src/test/java/StepsDefenition/LoginSteps.java
+++ tests/src/test/java/StepsDefenition/LoginSteps.java
@@ -1,38 +1,29 @@
 package StepsDefenition;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
+import PageObject.LoginPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import util.DriverManager;
 
 public class LoginSteps {
-    WebDriver driver;
-
+    WebDriver driver = DriverManager.getDriver();
+    LoginPage loginPage = new LoginPage(driver);
 
     @Given("I am in the login page")
-    public void an_empty_step() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-
-        driver.get("http://localhost:4200/");
-        driver.manage().window().maximize();
-
-        driver.findElement(By.xpath("//a[text()='Login']")).click();
+    public void openLoginPage() {
+        loginPage.openUrl();
+        loginPage.goToLoginPage();
     }
 
     @When("I fill the username field with {string} and {string}")
-    public void another_empty_step(String username, String password) {
-        driver.findElement(By.id("loginUsername")).sendKeys(username);
-        driver.findElement(By.id("loginPassword")).sendKeys(password);
-        driver.findElement(By.id("loginButton")).click();
+    public void sendCredentials(String username, String password) {
+        loginPage.enterLoginCredentials(username, password);
     }
 
     @Then("I am Successfully logged in")
-    public void yet_another_empty_step() {
-        // Implement your step here
+    public void isLoggedIn() {
     }
 }
