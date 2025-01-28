59d35c3a84d724bfd345f89822d6848b26d70889
Francisco Resendes <102703028+FranciscoResendes@users.noreply.github.com>
2025-01-28 23:13:14 +0000

    Added closeDriver method

--- tests/src/test/java/util/DriverManager.java
+++ tests/src/test/java/util/DriverManager.java
@@ -6,12 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverManager {
-    static WebDriver driver;
+    private static WebDriver driver;
 
     public static WebDriver getDriver() {
         WebDriverManager.chromedriver().driverVersion("132.0.6834.111").setup();
         driver = new ChromeDriver();
         return driver;
+    }
+
+    public static void closeDriver(WebDriver driver) {
+        driver.quit();
     }
     
 }
